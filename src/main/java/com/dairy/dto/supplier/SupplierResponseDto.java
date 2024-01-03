36679930ed8a601014118380616dd57b7c2adcb3
Dipti Wagh <diptiwagh111@gmail.com>
2024-01-03 11:46:15 +0530

    Supplier-Add_API

---
+++ src/main/java/com/dairy/dto/supplier/SupplierResponseDto.java
@@ -0,0 +1,28 @@
+package com.dairy.dto.supplier;
+
+import java.time.LocalDateTime;
+
+import com.dairy.dto.employee.EmployeeResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplierResponseDto {
+	private Long id;
+	private String name;
+	private String contact;
+	private LocalDateTime dateOfRegistration;
+	private String address;
+	private Long bankId;
+
+	private String bankName;
+	private String ifscCode;
+	private String bankBranchName;
+	private String accountNumber;
+	private String upiId;
+
+}
